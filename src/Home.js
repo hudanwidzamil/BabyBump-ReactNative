99205a2705c328a0eb1bb4ae935bad7b3144caf4
Hudan Widzamil <hudanwidzamil@gmail.com>
2021-03-18 22:10:55 +0700

    add for you icons

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {StyleSheet, View, Text } from 'react-native';
+import {StyleSheet, View, Text, Image, ScrollView } from 'react-native';
 
 function HomeScreen() {
     return (
-      <View style={{
+      <ScrollView style={{
         flex: 1,
         backgroundColor: '#fff',
       }}>
@@ -12,13 +12,32 @@ function HomeScreen() {
           <Text style={{fontSize:21,color:"#fff"}}>Day</Text>
           <Text style={{fontSize:18,color:"#fff"}}>days to your delivery</Text>
         </View>
-        <View style={{padding:32}}>
+        <View style={{paddingVertical:16, paddingHorizontal:32}}>
           <Text style={{fontSize:21}}>For You</Text>
+          <View style={{flexDirection:"row"}}>
+            <View style={{alignItems:"center", flex:1}}>
+              <Image source={require("../assets/shopicon/maternitywear.png")}/>
+              <Text style={{fontSize:11, textAlign:"center"}}>Maternity Wear</Text>
+            </View>
+            <View style={{alignItems:"center", flex:1}}>
+              <Image source={require("../assets/shopicon/supplement.png")}/>
+              <Text style={{fontSize:11, textAlign:"center"}}>Supplement</Text>
+            </View>
+            <View style={{alignItems:"center", flex:1}}>
+              <Image source={require("../assets/shopicon/stroller.png")}/>
+              <Text style={{fontSize:11, textAlign:"center"}}>Stroller</Text>
+            </View>
+            <View style={{alignItems:"center", flex:1}}>
+              <Image source={require("../assets/shopicon/babywear.png")}/>
+              <Text style={{fontSize:11, textAlign:"center"}}>Baby Wear</Text>
+            </View>
+            
+          </View>
         </View>
-        <View style={{padding:32}}>
+        <View style={{paddingVertical:16, paddingHorizontal:32}}>
           <Text style={{fontSize:21}}>Daily Reads</Text>
         </View>
-        <View style={{padding:32}}>
+        <View style={{paddingVertical:16, paddingHorizontal:32}}>
           <Text style={{fontSize:21}}>Trending in Social</Text>
           <View style={{flexDirection:"row", margin:5, borderBottomColor: '#888', borderBottomWidth: 1}}>
             <Text style={{fontSize:20, flex:0.1}}>#1</Text>
@@ -29,20 +48,19 @@ function HomeScreen() {
             <Text style={{fontSize:12, flex:1}}>Klo hamil lebih bagus makan ikan atau sapi ya?</Text>
           </View>
         </View>
-      </View>
-      
+      </ScrollView>
     );
   }
   
   const styles = StyleSheet.create({
     topBar:{
-      flex:0.25,
+      flex:0.5,
       backgroundColor: '#CAA8F5',
       alignItems: 'flex-start',
       justifyContent: 'flex-end',
       padding: 32,
       borderBottomLeftRadius:25,
-      borderBottomRightRadius:25
+      borderBottomRightRadius:25,
     },
   });
 
