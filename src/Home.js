c6ba2329f29f3e577d26f87a05e826f526c36f82
Hudan Widzamil <hudanwidzamil@gmail.com>
2021-03-19 15:15:10 +0700

    simplify Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import {StyleSheet, View, Text, Image, ScrollView } from 'react-native';
 
+const ForYouButton = (props) =>{
+  return(
+    <View style={{alignItems:"center", flex:1}}>
+      <Image source={props.imgPath}/>
+      <Text style={{fontSize:11, textAlign:"center", paddingTop:8}}>{props.name}</Text>
+    </View>
+  );
+}
+
+const TrendingText = (props) =>{
+  return(
+    <View style={{flexDirection:"row", borderBottomColor: '#888', borderBottomWidth: 1, paddingBottom:5, paddingTop:5}}>
+      <Text style={{fontSize:20, flex:0.125}}>#{props.no}</Text>
+      <Text style={{fontSize:12, flex:1}}>{props.isi}</Text>
+    </View>
+  );
+}
+
 function HomeScreen() {
     return (
       <ScrollView style={{
@@ -13,25 +31,12 @@ function HomeScreen() {
           <Text style={{fontSize:18,color:"#fff"}}>days to your delivery</Text>
         </View>
         <View style={{paddingVertical:16, paddingHorizontal:32}}>
-          <Text style={{fontSize:21}}>For You</Text>
+          <Text style={{fontSize:21, paddingBottom:8}}>For You</Text>
           <View style={{flexDirection:"row"}}>
-            <View style={{alignItems:"center", flex:1}}>
-              <Image source={require("../assets/shopicon/maternitywear.png")}/>
-              <Text style={{fontSize:11, textAlign:"center"}}>Maternity Wear</Text>
-            </View>
-            <View style={{alignItems:"center", flex:1}}>
-              <Image source={require("../assets/shopicon/supplement.png")}/>
-              <Text style={{fontSize:11, textAlign:"center"}}>Supplement</Text>
-            </View>
-            <View style={{alignItems:"center", flex:1}}>
-              <Image source={require("../assets/shopicon/stroller.png")}/>
-              <Text style={{fontSize:11, textAlign:"center"}}>Stroller</Text>
-            </View>
-            <View style={{alignItems:"center", flex:1}}>
-              <Image source={require("../assets/shopicon/babywear.png")}/>
-              <Text style={{fontSize:11, textAlign:"center"}}>Baby Wear</Text>
-            </View>
-            
+            <ForYouButton name="Maternity Wear" imgPath={require("../assets/shopicon/maternitywear.png")}/>
+            <ForYouButton name="Supplement" imgPath={require("../assets/shopicon/supplement.png")}/>
+            <ForYouButton name="Stroller" imgPath={require("../assets/shopicon/stroller.png")}/>
+            <ForYouButton name="Baby Wear" imgPath={require("../assets/shopicon/babywear.png")}/>
           </View>
         </View>
         <View style={{paddingVertical:16, paddingHorizontal:32}}>
@@ -39,14 +44,14 @@ function HomeScreen() {
         </View>
         <View style={{paddingVertical:16, paddingHorizontal:32}}>
           <Text style={{fontSize:21}}>Trending in Social</Text>
-          <View style={{flexDirection:"row", margin:5, borderBottomColor: '#888', borderBottomWidth: 1}}>
-            <Text style={{fontSize:20, flex:0.1}}>#1</Text>
-            <Text style={{fontSize:12, flex:1}}>Kenapa ya setiap pagi rasanya seluruh tubuhku kram,, apa itu normal??</Text>
-          </View>
-          <View style={{flexDirection:"row", margin:5, borderBottomColor: '#888', borderBottomWidth: 1}}>
-            <Text style={{fontSize:20, flex:0.1}}>#2</Text>
-            <Text style={{fontSize:12, flex:1}}>Klo hamil lebih bagus makan ikan atau sapi ya?</Text>
-          </View>
+          <TrendingText 
+            no="1" 
+            isi="Kenapa ya setiap pagi rasanya seluruh tubuhku kram,, apa itu normal??"
+          />
+          <TrendingText 
+            no="2" 
+            isi="Klo hamil lebih bagus makan ikan atau sapi ya?"
+          />
         </View>
       </ScrollView>
     );
