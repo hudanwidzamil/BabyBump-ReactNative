8ef800c8f8fe3055202394ae5ab3bb48d16a2f5c
Hudan Widzamil <hudanwidzamil@gmail.com>
2021-03-20 16:27:48 +0700

    add navbar icon

--- App.js
+++ App.js
@@ -10,14 +10,54 @@ import HomeScreen from './src/Home';
 
 const Tab = createBottomTabNavigator();
 
+const IconBottom = (props) => {
+  const { color, focused } = props.data
+  let colorSelected = focused ? color : 'grey'
+  return (
+      <View>
+          <Image source={props.image} style={{ width: 24, height: 26, tintColor: colorSelected }} />
+      </View>
+  )
+}
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name = "Home" component = {HomeScreen}/>
-        <Tab.Screen name = "Social" component = {SocialScreen}/>
-        <Tab.Screen name = "Shop" component = {ShopScreen}/>
-        <Tab.Screen name = "MyAccount" component = {MyAccountScreen}/>
+      <Tab.Navigator tabBarOptions={{
+        activeTintColor:'black'
+      }}>
+        <Tab.Screen name = "Home" component = {HomeScreen}
+          options={{
+            tabBarLabel: "Home",
+            tabBarIcon: (props) => (
+              <IconBottom data={props} image={require('./assets/navbaricon/home.png')}/>
+            )
+          }}
+        />
+        <Tab.Screen name = "Social" component = {SocialScreen}
+          options={{
+            tabBarLabel: "Social",
+            tabBarIcon: (props) => (
+              <IconBottom data={props} image={require('./assets/navbaricon/social.png')}/>
+            )
+          }}
+        />
+        <Tab.Screen name = "Shop" component = {ShopScreen}
+          options={{
+            tabBarLabel: "Shop",
+            tabBarIcon: (props) => (
+              <IconBottom data={props} image={require('./assets/navbaricon/shop.png')}/>
+            )
+          }}
+        />
+        <Tab.Screen name = "MyAccount" component = {MyAccountScreen}
+          options={{
+            tabBarLabel: "My Account",
+            tabBarIcon: (props) => (
+              <IconBottom data={props} image={require('./assets/navbaricon/myaccount.png')}/>
+            )
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
