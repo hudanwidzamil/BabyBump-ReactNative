c03c9fa4ed0cc5cf787e0bf91f79be004032271a
Hudan Widzamil <hudanwidzamil@gmail.com>
2021-05-16 09:29:12 +0700

    fix landing

--- src/auth/Landing.js
+++ src/auth/Landing.js
@@ -4,7 +4,7 @@ import { Text, View, Button } from 'react-native';
 function Landing({navigation}) {
     return (
         <View style={{flex:1, backgroundColor:"#55367BF0"}}>
-            <View style={{flex:1, justifyContent:"space-evenly", marginVertical:330, marginHorizontal:80}}>
+            <View style={{flex:1, justifyContent:"center",}}>
                 <Button
                     title="Register"
                     color="#CAA8F5"
